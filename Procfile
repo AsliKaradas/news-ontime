802c39b09d42d226b0fe10dac90d8764fe5e405c
Asli Karadas <karadas.asli@gmx.at>
2024-07-24 08:48:09 +0000

    Ensure all files are tracked

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mynews.wsgi:application
+web: gunicorn mynews.wsgi
--- mynews/mynews/wsgi.py
+++ mynews/mynews/wsgi.py
@@ -8,8 +8,25 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import logging
 from django.core.wsgi import get_wsgi_application
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+logger.debug('Starting WSGI application')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mynews.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    logger.debug('WSGI application loaded successfully')
+except Exception as e:
+    logger.error('Failed to load WSGI application', exc_info=e)
+    raise
+
