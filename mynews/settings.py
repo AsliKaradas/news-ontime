008b8fdead449a1926a64827f5bc04376bc1ff65
Asli Karadas <karadas.asli@gmx.at>
2024-07-24 09:17:29 +0000

    Add whitenoise and update requirements

--- mynews/settings.py
+++ mynews/settings.py
@@ -147,6 +147,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
